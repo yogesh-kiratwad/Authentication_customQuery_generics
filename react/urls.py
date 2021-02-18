1d7c8059f7c23ae9f4c99ef5ae6ec31fbc0c6ff4
Yogesh Kiratwad <60289228+yogesh-kiratwad@users.noreply.github.com>
2021-02-18 11:01:45 +0530

    Update urls.py

--- react/urls.py
+++ react/urls.py
@@ -23,6 +23,7 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls')),
+    path('auth/',include('rest_framework.urls')),
     path('api/token/',TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/',TokenRefreshView.as_view(), name='token_refresh'),
     path('docs/',include_docs_urls(title='BlogAPI')),
